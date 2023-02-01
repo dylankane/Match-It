c9a0afcd2ae834c96418632eaff07db09fb1689a
Dylan <dylankane3@gmail.com>
2023-02-01 22:37:43 +0000

    Add javascript code to record the flipped cards and compare them

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,40 @@
 const cards = document.querySelectorAll('.card');
 
+let flipped = false;
+let firstFlip;
+let secondFlip;
+
 function flipCard() {
-  this.classList.toggle('flip');
-}
+  this.classList.add('flip');
+  // flipped++;
+
+
+
+  if(flipped === false) {
+    firstFlip = this.getAttribute("data-image");
+    flipped = true;
+    } else {
+      secondFlip = this.getAttribute("data-image");
+
+      if (firstFlip === secondFlip) {
+        let matched = document.querySelectorAll('.card.flip');
+  
+        matched[0].classList.add('matched');
+        matched[1].classList.add('matched');
+          
+      }   
+
+    } 
+    // else {
+      // flipped = 1
+    // }
+
+    
+      console.log({flipped, firstFlip, secondFlip});
+      
+  }  
+  
+
 
 cards.forEach(card => card.addEventListener('click', flipCard));
 
