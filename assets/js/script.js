f889f585b0395f536d89345ca879320f41c12cb3
Dylan <dylankane3@gmail.com>
2023-02-03 12:37:04 +0000

    Fix reset functionality bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,5 @@
-
-
 const cards = document.querySelectorAll('.card');
-
+let moveCounter = document.getElementById('moves-counter');
 shuffle();
 
 
@@ -16,62 +14,61 @@ cards.forEach(card => card.addEventListener('click', flipCard));
 newGame.forEach(button => button.addEventListener('click', restart));
 
 function flipCard() {
-  
+
   if (lockBoard) return;
 
-    this.classList.add('flip');
+  this.classList.add('flip');
 
-    moves++;
-   
-      if(!flipped) {
-        firstFlip = this;
-        flipped = true;
+  moves++;
+  this.classList.add('disable');
 
-      } else {
-        secondFlip = this;
-        flipped = false;
-       
+  if (!flipped) {
+  
+    flipped = true;
+    firstFlip = this;
 
-        if (firstFlip.dataset.image === secondFlip.dataset.image) {
+  } else {
+    lockBoard = true;
+    flipped = false;
+    secondFlip = this;
     
-          firstFlip.removeEventListener('click', flipCard);
-          secondFlip.removeEventListener('click', flipCard);
 
-          setTimeout (() =>{
-          firstFlip.classList.add('matched');
-          secondFlip.classList.add('matched');
-          }, 700);
+    if (firstFlip.dataset.image === secondFlip.dataset.image) {
 
-        } else {
-          lockBoard = true;
+      
+      setTimeout(() => {
+        firstFlip.classList.add('matched');
+        secondFlip.classList.add('matched');
+        lockBoard = false;
+      }, 700);
 
-            setTimeout(() => {
-              firstFlip.classList.remove('flip');
-              secondFlip.classList.remove('flip');
-              //cards.removeEventListener('click', flipCard);
-              // document.getElementsByClassName('.cards').style.pointerEvents = "none";
-              lockBoard = false;
-          }, 1500);
+    } else {
+     
 
-        
+      setTimeout(() => {
+        firstFlip.classList.remove('flip','disable');
+        secondFlip.classList.remove('flip','disable');
+       
+        lockBoard = false;
+      }, 1500);
+
+
+
+    }
+  }
+  //console.log({flipped, firstFlip, secondFlip});
+
+  moveCounter.innerHTML = `${moves}`;
+}
+
+
+function restart() {
+  cards.forEach(card => {
+    card.classList.remove('flip', 'matched','disable');
   
-        }  
-      }  
-      //console.log({flipped, firstFlip, secondFlip});
-      let moveCounter = document.getElementById('moves-counter')
-      moveCounter.innerHTML = `${moves}`;
-}  
-
-
-function restart(){
-  
-  firstFlip.classList.remove('flip');
-  secondFlip.classList.remove('flip');
-  firstFlip.classList.remove('matched');
-  secondFlip.classList.remove('matched');
-  firstFlip.addEventListener('click', flipCard);
-  secondFlip.addEventListener('click', flipCard);
-  moves.innerHTML = 0
+  });
+  moves = 0;
+  moveCounter.innerHTML = 0;
 
   shuffle();
   clock();
@@ -80,30 +77,29 @@ function restart(){
 
 
 function shuffle() {
-  
+
   cards.forEach(panel => {
-      let randomize = Math.floor(Math.random() * 12);
-      panel.style.order = randomize;
-     
-    }
-  );
- 
+    let randomize = Math.floor(Math.random() * 12);
+    panel.style.order = randomize;
+
+  });
+
 }
 
 // Timer countdown
 
 let counter = document.getElementById('counter-span');
 let seconds = 5;
-let interval = setInterval(clock, 1000);   
+let interval = setInterval(clock, 1000);
 
 function clock() {
 
   seconds--;
-  counter.innerHTML =`${seconds}`
+  counter.innerHTML = `${seconds}`
 
   if (seconds <= 0) {
     clearInterval(interval);
     //alert("Time Is Up!!  Press 'restart'; to try again");
-    
+
   }
 }
