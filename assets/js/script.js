ce829ca8fec01252f6f03c96ea29dfca7f9c2897
Dylan <dylankane3@gmail.com>
2023-02-08 23:38:37 +0000

    dd comments and formatting to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,30 @@
-
-
 const cards = document.querySelectorAll('.card');
 
 shuffle();
 
-let matchCounter = 0
-let finalScore = document.getElementById('final-score')
-let moveCounter = document.getElementById('moves-counter');
-let moves = 0;
+let matchCounter = 0 /*set the match counter to 0*/
+
+let finalScore = document.getElementById('final-score') /* get the html element to display final score */
+let moveCounter = document.getElementById('moves-counter'); /* get the html element to display  current moves */
+let moves = 0; /* set moves counter to 0 */
+
 let flipped = false;
 let lockBoard = false;
 let firstFlip;
 let secondFlip;
-let newGame = document.querySelectorAll('.new-game');
+
 let totalTime = 45;
 let counter = document.getElementById('counter-span');
 let seconds = totalTime;
-let interval; 
+let interval;
 
-
+let newGame = document.querySelectorAll('.new-game');
 
 let timeModal = document.getElementById('time-modal')
 let winModal = document.getElementById('win-modal')
 
 cards.forEach(card => card.addEventListener('click', flipCard));
+
 newGame.forEach(button => button.addEventListener('click', restart));
 
 //Shuffle function
@@ -32,10 +33,11 @@ function shuffle() {
   cards.forEach(card => {
     let randomize = Math.floor(Math.random() * 12);
     card.style.order = randomize;
-  });}
+  });
+}
 
 // Timer countdown
-function activeGame (){
+function activeGame() {
   interval = setInterval(clock, 1000);
 }
 
@@ -45,10 +47,10 @@ function clock() {
   if (!seconds <= 0) {
     seconds--;
 
-  } else {  
+  } else {
     clearInterval(interval);
     timeModal.style.display = 'block';
-    window.onclick = function(event) {
+    window.onclick = function (event) {
       if (event.target == timeModal) {
         timeModal.style.display = "none";
       }
@@ -60,14 +62,14 @@ function clock() {
 
 function flipCard() {
 
- if (moves === 0) {
+  if (moves === 0) {
 
-  activeGame()
- }
+    activeGame()
+  }
 
 
   if (lockBoard) return;
-  
+
   this.classList.add('flip');
   this.classList.add('disable');
   moves++;
@@ -81,9 +83,9 @@ function flipCard() {
     lockBoard = true;
     flipped = false;
     secondFlip = this;
-    
 
-    if (firstFlip.dataset.image === secondFlip.dataset.image) {   
+
+    if (firstFlip.dataset.image === secondFlip.dataset.image) {
       setTimeout(() => {
         firstFlip.classList.add('matched');
         secondFlip.classList.add('matched');
@@ -91,59 +93,50 @@ function flipCard() {
       }, 700);
       matchCounter++
 
-        if (matchCounter === 6) {
-          setTimeout (() => {
-          winModal.style.display = 'block';}, 700);
-          
-          clearInterval(interval);
-          finalScore.innerHTML = `${moves}`;
+      if (matchCounter === 6) {
+        setTimeout(() => {
+          winModal.style.display = 'block';
+        }, 700);
 
-          window.onclick = function(event) {
-            if (event.target == winModal) {
-              winModal.style.display = "none";
-            }
+        clearInterval(interval);
+        finalScore.innerHTML = `${moves}`;
+
+        window.onclick = function (event) {
+          if (event.target == winModal) {
+            winModal.style.display = "none";
           }
         }
+      }
 
     } else {
-     
+
       setTimeout(() => {
-        firstFlip.classList.remove('flip','disable');
-        secondFlip.classList.remove('flip','disable');
-       
+        firstFlip.classList.remove('flip', 'disable');
+        secondFlip.classList.remove('flip', 'disable');
+
         lockBoard = false;
       }, 1500);
     }
   }
 }
 
+
+
 //Restart function
 
 function restart() {
-  cards.forEach(card => {card.classList.remove('flip', 'matched','disable');});
+  cards.forEach(card => {
+    card.classList.remove('flip', 'matched', 'disable');
+  });
 
   moves = 0;
   moveCounter.innerHTML = `${moves}`;
   matchCounter = 0;
-  
+
   clearInterval(interval);
   counter.innerText = totalTime;
-  seconds = totalTime; 
+  seconds = totalTime;
   timeModal.style.display = "none";
   winModal.style.display = "none";
   shuffle();
 }
-
-
-// window.onclick = function(event) {
-//   if (event.target == timeModal) {
-//     timeModal.style.display = "none";
-//   }
-// }
-
-window.onclick = function(event) {
-  if (event.target == winModal) {
-    winModal.style.display = "none";
-  }
-}
-
